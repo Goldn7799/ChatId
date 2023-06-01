4a2dc5d71747372c8e73573f354c93b1db0e621d
Goldn7799 <syeifsultan@gmail.com>
2023-06-01 13:00:29 +0700

    update structure

--- Rules.js
+++ Rules.js
@@ -13,17 +13,21 @@
   AuthCode : Auth Code to acces full information of user
   DisplayName : Name for display profile
   UserName : Identity as Public ID
-  Bio : User Bio
-  BlueTick : if 'none' meaning as no have blue tick, if true filled with blue tick type
+  Bio : User Bio [Default : "Iam Now Here"]
+  BlueTick : if 'none' meaning as no have blue tick, if true filled with blue tick type [Default : "no"]
   LastOnline : Data from Date.now() or TimeStamp
-  Region : Region Code
+  Region : Region Code [Default : "en_us"]
   PublicSettings : public settings 
+    ShowLastOnline : if true, all user can see last online this user [Default : true]
+    ShowProfilePicAsPublic : if true, all user can see profile this user [Default : true]
+    ShowOnSearch : if true, All user can see this user on search [Default : false]
+    ShowBannerAdPublic : if true, All user can show banner this user [Default : true]
   UserState : 
-    IsBanned : for false fill with 'no' id true fill with her reason
-    ContactList : Array Of Contact list AsNote for note and id for server id of user
-    GroupId : List of Grup user joined
-    BlockList : list array of server id user
-    PrivateChat : Array list of Frivate ChatId
+    IsBanned : for false fill with 'no' id true fill with her reason [Default : "no"]
+    ContactList : Array Of Contact list AsNote for note and id for server id of user [Default : []]
+    GroupId : List Array of Grup user joined [Default : []]
+    BlockList : list array of server id user [Default : []]
+    PrivateChat : Array list of Frivate ChatId [Default : []]
 */
 const UserData = {
   'usahebsahe': {
@@ -36,8 +40,8 @@ const UserData = {
     Regios: 'en_us',
     PublicSettings: {
       ShowLastOnline: false,
-      ShowProfileAsPublic: false,
-      ShowProfileOnSearch: false,
+      ShowProfilePicAsPublic: false,
+      ShowOnSearch: false,
       ShowBannerAsPublic: false
     },
     UserState: {
@@ -59,6 +63,22 @@ const UserData = {
 }
 
 // Group Data
+/*
+  Key : Group Id
+  Name : Group Name/Subject
+  Description : Group Description [Default : ""]
+  Owner : string server id user owner
+  PublicSettings :
+    AdminOnlyMessage : if true, member not admin can't send message [Default : false]
+    AdminOnlyEditGroup : if true, member cant edit group info or edit group [Default : true]
+    NewMemberConfirmation : if true, nre member need admin confirmation to join [Default : false]
+    ShowOnSearch : if true, group can showed on search [Default : false]
+  PendingMemberList : array of pending member if NewMemberConfirmation is true [Default : []]
+  GroupParticipants : Array Of Member List and Admin
+    Id : Server id of member user
+    IsAdmin : To Indicate User is Group admin or not
+  Messages : Array list of message [Default : []]
+*/
 const GroupData = {
   'jdaiuwbsha': {
     Name: 'Programmirer',
@@ -67,7 +87,8 @@ const GroupData = {
     PublicSettings: {
       AdminOnlyMessage: false,
       AdminOnlyEditGroup: true,
-      NewMemberConfirmation: false
+      NewMemberConfirmation: false,
+      ShowOnSearch: true
     },
     PendingMemberList: [],
     GroupParticipants: [
@@ -116,9 +137,9 @@ const PrivateChat = {
     Type: type of chat 'group'|'private'
     Id : id of chat
   Id : If of Message
-  Reply : Reply Id Message
+  Reply : Reply Id Message [Default : false]
   From : From who this message sent
-  Type : Type of chat(can see at message type and body)
+  Type : Type of chat(can see at message type and body) [Default : "text"]
   Body : Data list(can see at message type and body)
 */
 const StandartMessage = {
@@ -138,7 +159,7 @@ const TextMessage = {
 }
 
 /*
-  Type : image | sticker
+  Type : image | sticker [Default : image]
   Body :
     FileId : Id Of image in storage server
     Caption : string of text caption
