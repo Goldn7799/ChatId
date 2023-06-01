61d9a01aee875ee82c4ff04172e88ac8e2e91b65
Goldn7799 <syeifsultan@gmail.com>
2023-06-01 15:07:09 +0700

    ddos protection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const config = require('./Config');
 const RequestFilter = require('./library/App/RequestFilter');
+const Utility = require('./library/Utility/Utility');
 
 /* Setup Express */
 const app = express();
@@ -14,6 +15,17 @@ app.use(bodyParser.json({
 }));
 
 /* Get Method */
+app.get('*', (req, res)=>{
+  if (RequestFilter.checkRequest(req.rawHeaders)) {
+    req.next()
+  } else {
+    res.status(429).json({
+      success: false,
+      message: 'You make req over limit',
+    });
+  }
+})
+// template
 app.get('/', (req, res)=>{
   if (RequestFilter.checkRequest(req.rawHeaders)) {
     res.status(200).json({
@@ -29,6 +41,33 @@ app.get('/', (req, res)=>{
 });
 
 /* Post Method */
+app.post('*', (req, res)=>{
+  if (RequestFilter.checkRequest(req.rawHeaders)) {
+    req.next()
+  } else {
+    res.status(429).json({
+      success: false,
+      message: 'You make req over limit',
+    });
+  }
+})
+// Enctiption
+app.post('/md5enc/:salt/', (req, res)=>{
+  const { salt } = req.params;
+  const { data } = req.body;
+  if (salt && data) {
+    res.status(200).json({
+      success: true,
+      message: 'Success Encrypt',
+      data: Utility.md5.enc(salt, data)
+    })
+  } else {
+    res.status(403).json({
+      success: false,
+      message: 'Failed Encrypt'
+    })
+  }
+})
 
 /* Start Listen to network */
 app.listen(config.appConfig.PortUse, ()=>{
---
+++ testArea.js
@@ -0,0 +1,16 @@
+fetch('http://localhost:2022/md5enc/a', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    data: 'Hello'
+  })
+})
+.then(ress => { return ress.json() })
+.then(res => {
+  console.log(res)
+})
+.catch(err => {
+  console.log(err)
+})
